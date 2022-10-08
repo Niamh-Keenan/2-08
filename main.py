8658c4f58d1f3c7c0cbd2cab7194815ccf9a895c
NiamhK07 <113034875+Niamh-Keenan@users.noreply.github.com>
2022-10-08 01:00:00 +0000

    Expanded program

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Program to generate a random number between 1 and 6
 
-import random
+import random 
 
 print (random.randint(1,6))
 
 number = input("Enter a number from 1 to 6: ")
 
-if 
+x=random.randint 
+
+y=number
